8fbb2d4c4818f3f7382f2446e5e5e18768f44cd1
Vladyslav Shvedov <mail@vlad.codes>
2025-09-17 09:19:22 -0700

    Initial Commit

---
+++ JWHTC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JWHTC
+//
+//  Created by vsh on 2025-09-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JWHTC/JWHTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  JWHTCApp.swift
+//  JWHTC
+//
+//  Created by vsh on 2025-09-17.
+//
+
+import SwiftUI
+
+@main
+struct JWHTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
